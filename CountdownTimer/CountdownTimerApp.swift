f24cf9583628df1a18da25fc7c4ee44d827f9a25
Kirill Andreyev <kirandev@ukr.net>
2024-05-30 21:27:06 +0200

    Initial Commit

---
+++ CountdownTimer/CountdownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerApp.swift
+//  CountdownTimer
+//
+//  Created by Kyrylo Andreiev on 30.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
